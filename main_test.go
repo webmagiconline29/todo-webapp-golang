51c0e5e9529d41ca3542f29e7e52861843b2f79d
Dabeer Shaikh <dab8106@gmail.com>
2023-10-27 14:01:26 +0530

    updates unit test

--- main_test.go
+++ main_test.go
@@ -1,3 +1,5 @@
+// main_test.go
+
 package main
 
 import (
@@ -5,65 +7,12 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
-	"sync"
 )
 
 func TestAddTodo(t *testing.T) {
-	var mu sync.Mutex // Create a Mutex
-
-	mu.Lock()
-	defer mu.Unlock()
-
 	resetTodos()
 
-	// ... Rest of your test code ...
-}
-
-func TestTodoList(t *testing.T) {
-	var mu sync.Mutex // Create a Mutex
-
-	mu.Lock()
-	defer mu.Unlock()
-
-	resetTodos()
-
-}
-
-func TestTodoList(t *testing.T) {
-	// Reset todos before testing
-	todos = []string{"Task 1", "Task 2"}
-
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(todoList)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
-
-	expected := "Task 1"
-	if !strings.Contains(rr.Body.String(), expected) {
-		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
-	}
-
-	expected = "Task 2"
-	if !strings.Contains(rr.Body.String(), expected) {
-		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
-	}
-}
-
-func TestAddTodo(t *testing.T) {
-	resetTodos()
-
-	formData := "todo=New Task"
-
-	req, err := http.NewRequest("POST", "/add", strings.NewReader(formData))
+	req, err := http.NewRequest("POST", "/add", strings.NewReader("todo=TestTodo"))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -81,13 +30,34 @@ func TestAddTodo(t *testing.T) {
 		t.Errorf("Expected 1 todo, got %d", len(todos))
 	}
 
-	if todos[0] != "New Task" {
-		t.Errorf("Expected 'New Task', got '%s'", todos[0])
+	if todos[0] != "TestTodo" {
+		t.Errorf("Expected 'TestTodo', got '%s'", todos[0])
+	}
+}
+
+func TestTodoList(t *testing.T) {
+	resetTodos()
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(todoList)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "<h1>WebMagic TODO List</h1>"
+	if !strings.Contains(rr.Body.String(), expected) {
+		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
 
 func resetTodos() {
-	mu.Lock()
-	defer mu.Unlock()
 	todos = []string{}
 }
