321d0b50457c1bd3620cb260580ca8147ce64148
Dabeer Shaikh <dab8106@gmail.com>
2023-10-27 12:42:11 +0530

    updates unit test

--- main_test.go
+++ main_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 func TestAddTodo(t *testing.T) {
+	resetTodos()
+
 	req, err := http.NewRequest("POST", "/add", strings.NewReader("todo=TestTodo"))
 	if err != nil {
 		t.Fatal(err)
@@ -32,6 +34,8 @@ func TestAddTodo(t *testing.T) {
 }
 
 func TestTodoList(t *testing.T) {
+	resetTodos()
+
 	req, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -51,76 +55,7 @@ func TestTodoList(t *testing.T) {
 		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
-package main
 
-import (
-	"net/http"
-	"net/http/httptest"
-	"strings"
-	"testing"
-)
-
-func TestAddTodo(t *testing.T) {
-	// Reset the global todos variable before each test
-	todos = nil
-
-	// Create a new request with a POST method and form value
-	req, err := http.NewRequest("POST", "/add", strings.NewReader("todo=TestTodo"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create a new recorder to capture the response
-	rr := httptest.NewRecorder()
-
-	// Create a handler function for testing
-	handler := http.HandlerFunc(addTodo)
-
-	// Serve the request
-	handler.ServeHTTP(rr, req)
-
-	// Check if the handler returned the correct status code
-	if status := rr.Code; status != http.StatusSeeOther {
-		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusSeeOther)
-	}
-
-	// Check if the todo was added correctly
-	if len(todos) != 1 {
-		t.Errorf("Expected 1 todo, got %d", len(todos))
-	}
-
-	if todos[0] != "TestTodo" {
-		t.Errorf("Expected 'TestTodo', got '%s'", todos[0])
-	}
-}
-
-func TestTodoList(t *testing.T) {
-	// Reset the global todos variable before each test
+func resetTodos() {
 	todos = []string{}
-
-	// Create a new request with a GET method
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create a new recorder to capture the response
-	rr := httptest.NewRecorder()
-
-	// Create a handler function for testing
-	handler := http.HandlerFunc(todoList)
-
-	// Serve the request
-	handler.ServeHTTP(rr, req)
-
-	// Check if the handler returned the correct status code
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
-
-	// Check if the response body contains the expected HTML
-	expected := "<h1>TODO List</h1>"
-	if !strings.Contains(rr.Body.String(), expected) {
-		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
-	}
 }
